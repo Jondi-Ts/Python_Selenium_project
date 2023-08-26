439e6b7cf5645c451aa5e25d53671cff030768b7
Jondi-Ts <jondi.ts@gmail.com>
2023-08-26 14:18:13 +0300

    added login page with needed elements

---
+++ Main/PageObject/login_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_username_field(self):
+        return self.driver.find_element(By.XPATH, "//input[@name='username']")
+
+    def get_password_field(self):
+        return self.driver.find_element(By.XPATH, "//input[@name='password']")
+
+    def login_btn(self):
+        return self.driver.find_element(By.XPATH("//input[@class='button']"))
